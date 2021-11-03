23ce53fba19323b12207db13992a9088eddb8bd8
Diego Brito <diegorbrito9@gmail.com>
2021-11-03 01:17:47 -0300

    adicionando a rota

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import {BrowserRouter, Route} from 'react-router-dom'
 import Content from "./components/contentContainer/Content";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Content />
-      </>
+      <BrowserRouter>
+        <Route path="/" component={ Content } />
+      </BrowserRouter>
     );
   }
 }
